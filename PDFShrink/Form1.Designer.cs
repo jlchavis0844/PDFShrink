c04efbff0214b000dc9a90655802039c2d462d51
jchavis <jchavis@DBA>
2017-07-18 13:40:22 -0700

    Add project files.

---
+++ PDFShrink/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace PDFShrink
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDir = new System.Windows.Forms.Button();
+            this.lblDirectory = new System.Windows.Forms.Label();
+            this.btnFlatten = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.btnCont = new System.Windows.Forms.Button();
+            this.cbCopyFiles = new System.Windows.Forms.CheckBox();
+            this.cbMakeDir = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.btnCloseReaders = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDir
+            // 
+            this.btnDir.Location = new System.Drawing.Point(573, 13);
+            this.btnDir.Name = "btnDir";
+            this.btnDir.Size = new System.Drawing.Size(176, 23);
+            this.btnDir.TabIndex = 0;
+            this.btnDir.Text = "Choose Directory";
+            this.btnDir.UseVisualStyleBackColor = true;
+            this.btnDir.Click += new System.EventHandler(this.btnDir_Click);
+            // 
+            // lblDirectory
+            // 
+            this.lblDirectory.AutoSize = true;
+            this.lblDirectory.Location = new System.Drawing.Point(267, 13);
+            this.lblDirectory.MaximumSize = new System.Drawing.Size(300, 20);
+            this.lblDirectory.MinimumSize = new System.Drawing.Size(300, 20);
+            this.lblDirectory.Name = "lblDirectory";
+            this.lblDirectory.Size = new System.Drawing.Size(300, 20);
+            this.lblDirectory.TabIndex = 1;
+            this.lblDirectory.Text = "No directory Selected";
+            // 
+            // btnFlatten
+            // 
+            this.btnFlatten.Location = new System.Drawing.Point(269, 68);
+            this.btnFlatten.Name = "btnFlatten";
+            this.btnFlatten.Size = new System.Drawing.Size(480, 25);
+            this.btnFlatten.TabIndex = 2;
+            this.btnFlatten.Text = "Flatten and Print";
+            this.btnFlatten.UseVisualStyleBackColor = true;
+            this.btnFlatten.Click += new System.EventHandler(this.btnFlatten_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(268, 112);
+            this.lblMsg.MaximumSize = new System.Drawing.Size(300, 20);
+            this.lblMsg.MinimumSize = new System.Drawing.Size(300, 20);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(300, 20);
+            this.lblMsg.TabIndex = 3;
+            // 
+            // btnCont
+            // 
+            this.btnCont.Location = new System.Drawing.Point(269, 108);
+            this.btnCont.Name = "btnCont";
+            this.btnCont.Size = new System.Drawing.Size(480, 23);
+            this.btnCont.TabIndex = 4;
+            this.btnCont.Text = "Finish Printing";
+            this.btnCont.UseVisualStyleBackColor = true;
+            this.btnCont.Click += new System.EventHandler(this.btnCont_Click);
+            // 
+            // cbCopyFiles
+            // 
+            this.cbCopyFiles.AutoSize = true;
+            this.cbCopyFiles.Location = new System.Drawing.Point(271, 37);
+            this.cbCopyFiles.Name = "cbCopyFiles";
+            this.cbCopyFiles.Size = new System.Drawing.Size(165, 17);
+            this.cbCopyFiles.TabIndex = 5;
+            this.cbCopyFiles.Text = "Copy Files to Small Directory?";
+            this.cbCopyFiles.UseVisualStyleBackColor = true;
+            // 
+            // cbMakeDir
+            // 
+            this.cbMakeDir.AutoSize = true;
+            this.cbMakeDir.Checked = true;
+            this.cbMakeDir.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbMakeDir.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.cbMakeDir.Location = new System.Drawing.Point(481, 37);
+            this.cbMakeDir.Name = "cbMakeDir";
+            this.cbMakeDir.Size = new System.Drawing.Size(153, 17);
+            this.cbMakeDir.TabIndex = 6;
+            this.cbMakeDir.Text = "Create small_files subfolder";
+            this.cbMakeDir.UseMnemonic = false;
+            this.cbMakeDir.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(248, 316);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Set Printer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Location = new System.Drawing.Point(272, 171);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.Size = new System.Drawing.Size(477, 200);
+            this.tbConsole.TabIndex = 9;
+            // 
+            // btnCloseReaders
+            // 
+            this.btnCloseReaders.Location = new System.Drawing.Point(269, 142);
+            this.btnCloseReaders.Name = "btnCloseReaders";
+            this.btnCloseReaders.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnCloseReaders.Size = new System.Drawing.Size(480, 23);
+            this.btnCloseReaders.TabIndex = 10;
+            this.btnCloseReaders.Text = "Close All Reader Windows";
+            this.btnCloseReaders.UseVisualStyleBackColor = true;
+            this.btnCloseReaders.Click += new System.EventHandler(this.btnCloseReaders_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 381);
+            this.Controls.Add(this.btnCloseReaders);
+            this.Controls.Add(this.tbConsole);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cbMakeDir);
+            this.Controls.Add(this.cbCopyFiles);
+            this.Controls.Add(this.btnCont);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnFlatten);
+            this.Controls.Add(this.lblDirectory);
+            this.Controls.Add(this.btnDir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PDF Mass Printer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDir;
+        private System.Windows.Forms.Label lblDirectory;
+        private System.Windows.Forms.Button btnFlatten;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Button btnCont;
+        private System.Windows.Forms.CheckBox cbCopyFiles;
+        private System.Windows.Forms.CheckBox cbMakeDir;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbConsole;
+        private System.Windows.Forms.Button btnCloseReaders;
+    }
+}
+
---
+++ PDFShrink/Form1.cs
@@ -0,0 +1,207 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace PDFShrink
+{
+    public partial class Form1 : Form
+    {
+        private string cwd = "";
+        private List<string> pdfs = new List<string>();
+
+        private void listAllPrinters()
+        {
+            foreach (var item in PrinterSettings.InstalledPrinters)
+            {
+                this.listBox1.Items.Add(item.ToString());
+            }
+            string defaultPrinter = new PrinterSettings().PrinterName;
+            tbConsole.AppendText("Default printer is " + defaultPrinter + "\n");
+            this.listBox1.SelectedIndex = this.listBox1.FindString(defaultPrinter);
+        }
+
+        public static class myPrinters
+        {
+            [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern bool SetDefaultPrinter(string Name);
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            listAllPrinters();
+            btnFlatten.Hide();
+            btnCont.Hide();
+        }
+
+        private void btnDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = "T:\\New Plan Document Roll Out\\Plan Document roll out\\School Districts";
+            if(fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                cwd = fbd.SelectedPath;
+                lblDirectory.Text = cwd;
+            }
+            if(cwd != null && cwd != "")
+            {
+                btnFlatten.Show();
+            }
+            tbConsole.AppendText("CWD: " + cwd + "\n");
+        }
+
+        private void GetPDFfiles(string target)
+        {
+            pdfs = new List<string>();
+
+            string[] files = System.IO.Directory.GetFiles(target);
+
+            foreach(string file in files)
+            {
+                if(file.Substring(file.Length - 4) == ".pdf")
+                {
+                    pdfs.Add(file);
+                    Console.WriteLine(file);
+                }
+            }
+        }
+
+        private void Flatten(List<string> pdfs)
+        {
+            foreach(string pdf in pdfs){
+                string outFile = pdf.Replace(".pdf", "-flat.pdf");
+                using (var existingFileStream = new FileStream(pdf, FileMode.Open))
+                {
+                    using (var newFileStream = new FileStream(outFile, FileMode.Create))
+                    {
+                        var pdfReader = new PdfReader(existingFileStream);
+                        var stamper = new PdfStamper(pdfReader, newFileStream);
+
+                        var form = stamper.AcroFields;
+
+                        stamper.FormFlattening = true;
+                        stamper.SetFullCompression();
+                        //form.GenerateAppearances = true;
+                        stamper.Close();
+                        pdfReader.RemoveUnusedObjects();
+                        pdfReader.Close();
+                    }
+                }
+            }
+        }
+
+        private void SendToPrinter(string file)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.Verb = "print";
+            info.FileName = file;
+            info.CreateNoWindow = true;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Process p = new Process();
+            p.StartInfo = info;
+            p.Start();
+
+            p.WaitForInputIdle();
+            System.Threading.Thread.Sleep(3000);
+            if (false == p.CloseMainWindow())
+                try
+                {
+                    p.Kill();
+                } catch (Exception e)
+                {
+                    tbConsole.AppendText("Exception: " + e + "\n");
+                    Console.WriteLine(e);
+                }
+
+            tbConsole.AppendText("printing " + Path.GetFileName(file) + "\n");
+        }
+
+        private void PrintAndPop()
+        {
+            SendToPrinter(pdfs.First());
+            pdfs.RemoveAt(0);
+
+            if(pdfs.Count == 0)
+            {
+                btnFlatten.Hide();
+                btnCont.Hide();
+            }
+        }
+
+        private void btnFlatten_Click(object sender, EventArgs e)
+        {
+            btnFlatten.Hide();
+            btnCont.Show();
+            List<string> files = new List<string>();
+            string newFile = "";
+            GetPDFfiles(cwd);
+            string newPath = Path.GetDirectoryName(pdfs.First()) + "\\small_files";
+
+            if (cbMakeDir.Checked)
+            {
+                Console.WriteLine(newPath);
+                System.IO.Directory.CreateDirectory(newPath);
+                tbConsole.AppendText("Made dir: " + newPath + "\n");
+            }
+
+            if (cbCopyFiles.Checked)
+            {
+                foreach (string file in pdfs)
+                {
+                    newFile = newPath + "\\" + Path.GetFileName(file);
+                    files.Add(newFile);
+                    System.IO.File.Copy(file, newFile, true);
+                }
+
+                pdfs = files;
+            }
+            PrintAndPop();
+            lblMsg.Text = "Done Flattening Docs";
+        }
+
+        private void btnCont_Click(object sender, EventArgs e)
+        {
+            CloseProcesses();
+
+            foreach (string pdf in pdfs)
+            {
+                SendToPrinter(pdf);
+            }
+
+            btnFlatten.Hide();
+            btnCont.Hide();
+            lblDirectory.Text = "Please select another directory!";
+            tbConsole.AppendText("All files have been processed!\n");
+            tbConsole.AppendText("Closing reader\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pname = this.listBox1.SelectedItem.ToString();
+            myPrinters.SetDefaultPrinter(pname);
+        }
+
+        private void CloseProcesses()
+        {
+            Process[] processes = Process.GetProcessesByName("AcroRd32");
+            for (int i = 0; i < processes.Length; i++)
+            {
+                Console.WriteLine(processes[i].ProcessName);
+                processes[i].CloseMainWindow();
+            }
+        }
+
+        private void btnCloseReaders_Click(object sender, EventArgs e)
+        {
+            CloseProcesses();
+        }
+    }
+}
